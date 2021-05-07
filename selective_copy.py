db2e8ca4554a8f7b6ae297c881a543593abd604b
piterczak <piterczak@gmail.com>
2021-05-07 10:39:10 +0200

    Dodano opisy programu selective_copy.py

--- selective_copy.py
+++ selective_copy.py
@@ -7,16 +7,16 @@ import os, shutil, re
 
 #tworzenie regexu dopasowujacego pliki z zakonczeniem .txt
 data_pattern = re.compile(r"""^(.*?) #cały tekst przed rozszerzeniem
-    (\.py)$
+    (\.py)$     #wybrane rozszerzenie pliku
     """,re.VERBOSE)
 
-p = r'C:\\Python\\'
-os.makedirs('C:\\Python\\kopia_zapasowa')
-for filename in os.listdir('.'):
+p = r'C:\\Python\\'     #okreslenie w jakim folderze beda pliki
+os.makedirs('C:\\Python\\kopia_zapasowa')       #tworzenie folderu kopii zapasowej
+for filename in os.listdir('.'):        #   os.listdir('.') tworzy liste string wszystkich plików znajdujących się w danym folderze (.) oznacza current working directory
     mo = data_pattern.search(filename)
-
+    # mo = matching object - sprawdzenie czy dany plik zgadza sie z naszymi wymaganiami co do rozszerzenia
     if mo == None:
-        continue
+        continue            #warunek powodujacy, ze gdy MO jest równe NONE czyli plik nie pasuje do rozszerzenia to pomijamy go i przechodzimy do kolejnego pliku z pętli
     shutil.copy(p + filename,  'C:\\Python\\kopia_zapasowa\\')
     print('Skopiowano ' + filename + ' do folderu kopia zapasowa' )
 
